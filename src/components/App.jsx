b9b74f814d929304eb3055dae917e8724a83b6bf
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-05-13 03:31:19 -0400

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,53 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+const api = "https://dog.ceo/api/breeds/image/random"
+
+function App() {
+
+    const [image, setImage] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+
+        fetchDog()
+
+    }, [])
+
+    function fetchDog() {
+
+        setLoading(true)
+
+        fetch(api)
+            .then(r => {
+
+                if (!r.ok) { console.log("ERROR") }
+
+                return r.json()
+
+            })
+            .then(data => {
+                setLoading(false)
+                setImage(data.message)
+            })
+
+    }
+
+
+    return (
+
+        <>
+
+            <div>
+                {loading ? <p>Loading...</p> : <img src={image} />}
+                <button onClick={fetchDog} >Fetch Dog</button>
+
+            </div>
+
+        </>
+
+    )
+
+}
+
+export default App
